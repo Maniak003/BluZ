05c9d6516a18ca929d4470d2fa3574696c775a97
Maniak003 <ed@ultrastar.ru>
2025-12-21 16:06:15 +0300

    изменено:      stm32cubeide/BluZ_LPM/Core/Inc/main.h 	изменено:      stm32cubeide/BluZ_LPM/Release/BluZ_LPM.elf

--- stm32cubeide/BluZ_LPM/Core/Inc/main.h
+++ stm32cubeide/BluZ_LPM/Core/Inc/main.h
@@ -275,7 +275,7 @@ void NotifyAct(uint8_t SRC, uint32_t repCnt);
  */
 #define ADC_VREF 2.8f									// Напряжение питания.
 //#define ADC_VREF_COEF 4.2f / 4080.0f
-static const float ADC_VREF_COEF = 4.2f / 3970.0f;		// Коэффициент завист от делителя напряжения. Нужно подбирать на полностью заряженном аккумуляторе.
+static const float ADC_VREF_COEF = 4.2f / 4055.68f;		// Коэффициент завист от делителя напряжения. Нужно подбирать на полностью заряженном аккумуляторе.
 
 /* USER CODE END Private defines */
 
